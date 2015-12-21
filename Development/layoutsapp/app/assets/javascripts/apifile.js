a4492b4e5af22285ae19ce3728501180791f53b7
miwllersalvador <m.salvador.barbosa@gmail.com>
2015-12-21 22:56:26 +0000

    tarea de HTML

---
+++ Development/layoutsapp/app/assets/javascripts/apifile.js
@@ -0,0 +1,83 @@
+function comenzar(){
+	zonadatos = document.getElementById("zonadatos");
+	var boton = document.getElementById("boton");
+	boton.addEventListener("click",modificar,false);
+	navigator.webkitPersistentStorage.requestQuota(5*1024*1024,acceso);
+}
+
+function acceso(){
+	window.webkitRequestFileSystem(PERSISTENT,5*1024*1024,crearsis,errores);
+}
+
+function crearsis(sistema){
+	espacio = sistema.root;
+	ruta = "";
+	mostrar();
+}
+
+function modificar(){
+	var origen = document.getElementById("archivo_origen").value;
+	var destino = document.getElementById("directorio_destino").value;
+	espacio.getFile(origen,null,function(archivo){
+		espacio.getDirectory(destino,null,function(directorio){
+			archivo.moveTo(directorio,null,exito,errores);
+		},errores);
+	},errores);
+}
+
+function exito(){
+	document.getElementById("archivo_origen").value = "";
+	document.getElementById("directorio_destino").value = "";
+	mostrar();
+}
+
+function mostrar(){
+	document.getElementById("entrada").value = "";
+	zonadatos.innerHTML = "";
+	espacio.getDirectory(ruta, null, leerdir, errores);
+}
+
+function leerdir(dir){
+	lector = dir.createReader();
+	leer();
+}
+
+function leer(){
+	lector.readEntries(function(archivos){
+		if(archivos.length){
+			listar(archivos);
+			}
+		},errores);
+}
+
+function listar(archivos){
+	for(var i=0; i<archivos.length; i++){
+		if(archivos[i].isFile){
+			zonadatos.innerHTML+=archivos[i].name+"<br>";
+		}
+		else if(archivos[i].isDirectory){
+			zonadatos.innerHTML+="<span onclick='cambiardir(\"" + archivos[i].name + "\")' class='directorio'>" + archivos[i].name + "</span><br>";
+		}
+	}
+}
+
+function cambiardir(nuevaruta){
+	ruta = ruta + nuevaruta + "/";
+	mostrar();
+}
+
+function volver(){
+	espacio.getDirectory(ruta,null,function(dir_actual){
+		dir_actual.getParent(function(dir_padre){
+			ruta=dir_padre.fullPath;
+			mostrar();
+		},errores);
+	},errores);
+}
+
+
+function errores(e){
+	alert("Ha habido un error: " + e.code);
+}
+
+window.addEventListener("load",comenzar,false);
